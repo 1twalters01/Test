2abe9acfe9c94fe5405e500b9ff0f11cbb57bb23
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-02-15 12:16:01 +0000

    Update main.rs

--- actix/solid_app/Backend/src/main.rs
+++ actix/solid_app/Backend/src/main.rs
@@ -11,9 +11,13 @@ async fn main() -> std::io::Result<()> {
             .service(routes::routes::app)
             .service(routes::routes::not_found)
             .service(routes::routes::index)
-            // .service(
-            //     web::scope("/api")
-            // )
+            
+            .service(
+                web::scope("/api")
+                .service(account::routes::login)
+                .service(account::routes::logout)
+                .service(account::register)
+            )
     })
   .bind("127.0.0.1:8080")?
   .run()
