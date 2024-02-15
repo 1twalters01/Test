59fbf9e1f3cb3ff4333af5e1e84c72b4674590cd
1twalters01 <1twalters01>
2024-02-15 01:00:56 +0000

    actix(rust) and solid(js) example template part finished

---
+++ actix/solid_app/Backend/src/main.rs
@@ -0,0 +1,21 @@
+mod routes;
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(routes::routes::script)
+            .service(routes::routes::css)
+            .service(routes::routes::about)
+            .service(routes::routes::app)
+            .service(routes::routes::not_found)
+            .service(routes::routes::index)
+            // .service(
+            //     web::scope("/api")
+            // )
+    })
+  .bind("127.0.0.1:8080")?
+  .run()
+  .await
+}
---
+++ actix/solid_app/Backend/src/routes/routes.rs
@@ -0,0 +1,74 @@
+use actix_web::{get, HttpResponse, Responder, Result, web};
+use std::{
+    fs,
+    path::PathBuf,
+};
+
+#[get("/favicon.ico")]
+async fn favicon() -> Result<HttpResponse> {
+    let image_content = web::Bytes::from(fs::read("../Frontend/public/favicon.ico").unwrap());
+    Ok(HttpResponse::Ok()
+        .content_type("image/png")
+        .body(image_content))
+}
+
+#[get("/{param:.*?}")]
+async fn index() -> Result<impl Responder> {
+    let index_path: PathBuf = "../Frontend/dist/index.html".into(); 
+    let index = web::Bytes::from(fs::read(index_path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(index))
+}
+
+#[get("/script.jsx")]
+async fn script() -> Result<impl Responder> {
+    let script_path: PathBuf = "../Frontend/dist/app.bundle.js".into();
+    let script_data = web::Bytes::from(fs::read(script_path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/javascript; charset=utf-8")
+        .body(script_data))
+}
+
+#[get("/src_App_jsx.app.bundle.js")]
+async fn app() -> Result<impl Responder> {
+    let script_path: PathBuf = "../Frontend/dist/src_App_jsx.app.bundle.js".into();
+    let script_data = web::Bytes::from(fs::read(script_path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/javascript; charset=utf-8")
+        .body(script_data))
+}
+
+#[get("/src_NotFound_jsx.app.bundle.js")]
+async fn not_found() -> Result<impl Responder> {
+    let script_path: PathBuf = "../Frontend/dist/src_NotFound_jsx.app.bundle.js".into();
+    let script_data = web::Bytes::from(fs::read(script_path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/javascript; charset=utf-8")
+        .body(script_data))
+}
+
+#[get("/src_About_jsx.app.bundle.js")]
+async fn about() -> Result<impl Responder> {
+    let script_path: PathBuf = "../Frontend/dist/src_About_jsx.app.bundle.js".into();
+    let script_data = web::Bytes::from(fs::read(script_path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/javascript; charset=utf-8")
+        .body(script_data))
+}
+
+#[get("/src_App_module_css.app.bundle.js")]
+async fn css() -> Result<impl Responder> {
+    let script_path: PathBuf = "../Frontend/dist/src_App_module_css.app.bundle.js".into();
+    let script_data = web::Bytes::from(fs::read(script_path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/javascript; charset=utf-8")
+        .body(script_data))
+}
+
